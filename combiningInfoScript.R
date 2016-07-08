60f5ba57aad3d8dd4d4de8b5cf8f6cfac8727844
Diane Menuz <dmenuz@utah.gov>
2016-07-08 10:39:45 -0600

    much improved, fixed issue with multiple versions of each taxa and habitat indicator

--- combiningInfoScript.R
+++ combiningInfoScript.R
@@ -16,7 +16,7 @@ cols=c("assess", "wildlife", "mollusks", "amphib", "waterfowl", "shorebirds", "w
 cols=c("wildlife", "mollusks", "amphib",  "sagegrouse", "salmonids", "rapots", "songbirds")
 
 resultsFrame=as.data.frame.matrix(table(finalDat$UniqueID, finalDat[,cols[1]]))
-resultsFrame$dataType=cols[1]
+resultsFrame$dataType="test"
 resultsFrame$UniqueID=row.names(resultsFrame)
 outResultsFrame=resultsFrame
 
@@ -27,8 +27,10 @@ for (i in 2:length(cols)){
   outResultsFrame=rbind(resultsFrameTemp, outResultsFrame)
 }
 
-
-outResults=merge(outResultsFrame, dat1[,c("UniqueID", "category", "indicator")], by="UniqueID")
+mergeDat=read.csv(fileList[1], header=TRUE, skip=2)
+outResults=merge(outResultsFrame, mergeDat[,c("UniqueID", "category", "indicator")], by="UniqueID")
+head(outResults)
+dim(outResults)
 setwd("U:\\GWP\\Wetland\\CoreIndicatorsGrant\\wildlifeIndicatorChecklists\\summarizedChecklistOutput\\")
 
 write.csv(outResults, "wildlifeSummaries.csv")
@@ -36,6 +38,9 @@ write.csv(outResults, "wildlifeSummaries.csv")
 
 assess1=as.data.frame.matrix(table(finalDat$UniqueID, finalDat$assess))
 assess1$UniqueID=row.names(assess1)
-assess2=merge(assess1, dat1[,c("UniqueID", "category", "indicator")], by="UniqueID")
-write.csv(assess2, "summarizedOutput\\canYouAssess.csv")
+assess2=merge(assess1, mergeDat[,c("UniqueID", "category", "indicator")], by="UniqueID")
+head(assess2)
+dim(assess2)
+write.csv(assess2, "canYouAssess.csv")
+
 
